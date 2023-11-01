66ab88d80443aba7aa5cbfece29379aeb33731fe
Crowdbotics <team@crowdbotics.com>
2023-11-01 10:51:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dili-toli-44263",
+  url: "https://dili-toli-44263.botics.co",
+  api: "https://dili-toli-44263.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
